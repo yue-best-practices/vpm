5713324d537838b0b07c748d86c150303a47cafe
Yuanjianxin <1814888800@qq.com>
2019-12-17 18:22:09 +0800

    remove comment code

--- array.v
+++ array.v
@@ -3,19 +3,6 @@
 */
 module main
 
-// []string 截取  数组已经有.slice .left .right方法可以使用，自己写的方法弃用
-// fn string_array_slice(arr []string,start,end int)[]string{
-//     mut res:=[]string
-//     if start>=end || start>=arr.len{
-//         return res
-//     }
-//     for start<arr.len && start<end{
-//         res<<arr[start]
-//         start++
-//     }
-//     return res
-// }
-
 // []string 去重
 fn string_array_distinct(arr []string)[]string{
     mut res:=[]string
